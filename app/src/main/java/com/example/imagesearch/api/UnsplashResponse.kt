698ec40234c4a291ba89c7aca006d6c1272e814a
Mihir Kelkar <mihirk107@gmail.com>
2021-01-08 18:24:44 -0600

    Added the API and DI code

---
+++ app/src/main/java/com/example/imagesearch/api/UnsplashResponse.kt
@@ -0,0 +1,8 @@
+package com.example.imagesearch.api
+
+import com.example.imagesearch.data.UnsplashPhoto
+
+data class UnsplashResponse(
+    val results: List<UnsplashPhoto>
+) {
+}
--- app/src/main/java/com/example/imagesearch/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/imagesearch/ui/gallery/GalleryFragment.kt
@@ -2,6 +2,8 @@ package com.example.imagesearch.ui.gallery
 
 import androidx.fragment.app.Fragment
 import com.example.imagesearch.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class GalleryFragment: Fragment(R.layout.fragment_gallery) {
 }
